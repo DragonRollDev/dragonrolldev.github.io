83ced8732159443cf8cc7684c02c6a4078a669fd
Helton Leal <helton.danilo@gmail.com>
2025-10-27 19:43:11 -0300

    first distribution

---
+++ assets/tilesets/background2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="background2" tilewidth="8" tileheight="8" tilecount="7200" columns="120">
+ <image source="./images/background2.png" width="960" height="480"/>
+</tileset>
